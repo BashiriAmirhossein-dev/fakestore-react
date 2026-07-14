1371a460a75de6d4972d0d04bac0152212fae97d
BashiriAmirhossein-dev <bashiriamirhossein.dev@gmail.com>
2026-07-13 20:12:17 -0700

    chore: configure React Query provider

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,14 @@ import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import router from './app/router.jsx'
 
+import AppProviders from './app/providers.jsx'
+
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <AppProviders>
+      <RouterProvider router={router} />
+    </AppProviders>
   </StrictMode>,
 )
